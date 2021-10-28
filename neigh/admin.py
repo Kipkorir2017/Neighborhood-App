717ebbddc74ec56c83481d535e101c95688be37c
Kipkorir2017 <pkobenj41@gmail.com>
2021-10-28 23:36:33 +0300

    register models in the admin file

--- neigh/admin.py
+++ neigh/admin.py
@@ -1,3 +1,8 @@
+from .models import Business, Neighbourhood, Post, Profile
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Neighbourhood)
+admin.site.register(Post)
+admin.site.register(Business)
