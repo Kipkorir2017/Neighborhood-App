c0f02ed3782805affb77b17c6e96974d1ea0ceb2
Kipkorir2017 <pkobenj41@gmail.com>
2021-10-29 12:03:10 +0300

    add changes to templates and view

--- neigh/forms.py
+++ neigh/forms.py
@@ -1,1 +1,45 @@
-from django.contrib import  forms
+from django.contrib.auth.models import User
+from django import forms
+from .models import Business, Neighbourhood, Post, Profile
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', )
+
+class UpdateUserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('username',)
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['email','profile_pic', 'bio','neighbourhood']
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['title', 'image','content','created']
+
+class NewHoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        exclude = ('admin',)
+
+class NewBusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('user',)
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user',)
